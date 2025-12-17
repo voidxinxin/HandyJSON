89e7dca26e50633faadb0f127195c0e2b867765d
voidxin <voidxin@163.com>
2025-12-17 14:21:08 +0800

    修改值类型被本地格式化的问题

--- Source/BuiltInBasicType.swift
+++ Source/BuiltInBasicType.swift
@@ -118,6 +118,7 @@ fileprivate let formatter: NumberFormatter = {
     formatter.usesGroupingSeparator = false
     formatter.numberStyle = .decimal
     formatter.maximumFractionDigits = 16
+    formatter.decimalSeparator = "."
     return formatter
 }()
 
